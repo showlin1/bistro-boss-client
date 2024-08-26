354abe7565c027c66262de1590af1ce0426cb500
unknown <showlinakter77@gmail.com>
2024-08-27 01:35:44 +0600

    create a menue page

---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet-async';
+
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
+            <h2>Mennu</h2>
+        </div>
+    );
+};
+
+export default Menu;
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
+import {HelmetProvider } from 'react-helmet-async';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='max-w-screen-xl mx-auto'>
-      <RouterProvider router={router} />
-    </div>
+    <HelmetProvider>
+      <div className='max-w-screen-xl mx-auto'>
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
   </StrictMode>,
 )
